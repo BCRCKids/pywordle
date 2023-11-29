c62dbdd5a7b6b1eb4b585177702b75a9761e7dd2
BCRCKids <bellinghamcodingclub@gmail.com>
2023-11-28 19:08:17 -0800

    Update wordle.py
    Better comments

--- wordle.py
+++ wordle.py
@@ -1,6 +1,7 @@
 import random
 from termcolor import colored
 
+#Define print functions to print out colored letters
 def print_green (text):
     print_text = colored (text, "white", "on_green")
     print (print_text, end="")
@@ -10,6 +11,7 @@ def print_red (text):
 def print_yellow (text):
     print_text = colored (text, "white", "on_yellow")
     print (print_text, end="")
+
 guesses = 5
 lines = []
 #Read the words from the words file
@@ -43,13 +45,17 @@ while True:
             print_red(givenWord[i])
     print ("")
     
+    #decrement the user's remaining gueses
     guesses -= 1
 
+    #If they guessed it right, nice!
     if givenWord == random_word :
         print ("You guessed it!")
         break
+    #If they're out of guesses
     elif guesses == 0:
         print ("Out of guesses! The word was: " + random_word)
         break
+    #Otherwise, continue the game!
     else:
         print ("Not Quite! Guesses remaining: " + str(guesses))
